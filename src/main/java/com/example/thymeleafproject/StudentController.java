9067acb6c22dc867473a1fa773f5e0c8874a53f0
Eric Abell <ericabell@users.noreply.github.com>
2017-09-25 12:41:34 -0500

    Finished project. Last part was getting the view_student page to work

--- src/main/java/com/example/thymeleafproject/StudentController.java
+++ src/main/java/com/example/thymeleafproject/StudentController.java
@@ -16,12 +16,22 @@ public class StudentController {
     }
 
     @RequestMapping("/create_student")
-    public String createStudent(@RequestParam(value="first_name") String firstName, /* @RequestParam for last name */, /* @RequestParam for grade */, Model model) {
+    public String createStudent(@RequestParam(value="first_name") String firstName, @RequestParam(value="last_name") String lastName, @RequestParam(value="grade") Grade grade, Model model) {
         Student student = new Student();
 
+        System.out.println("RequestParams:");
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(grade);
+
         /* set student firstName, lastName and grade using the http request parameters */
+        student.setFirstName(firstName);
+        student.setLastName(lastName);
+        student.setGrade(grade);
 
         /* add the student to the model that will be used by the view_student html file */
+        model.addAttribute("student", student);
+
 
         return "view_student";
     }
